c01c21d4b3e10e3ea97ba0c1345178d7a301b581
mmaquer2 <mike.maquera212@gmail.com>
2024-11-25 17:26:34 -0500

    testing test workflow

---
+++ server/__tests__/example.test.js
@@ -0,0 +1,10 @@
+describe('Sample Tests', () => {
+    test('should print Hello World', () => {
+      const str = 'Hello World';
+      expect(str).toBe('Hello World');
+    });
+  
+    test('1 + 1 should equal 2', () => {
+      expect(1 + 1).toBe(2);
+    });
+  });
